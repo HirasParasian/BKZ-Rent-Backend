df24e80e7bf6bfa4f896b8ed9b370d0db8e6ff52
HirasParasian <hirasparasian@gmail.com>
2022-02-09 23:43:18 +0700

    Add : Response Helper

---
+++ src/helpers/response.js
@@ -0,0 +1,20 @@
+const response = (res, message, results, stats=200)=> {
+    let success = true
+    if(stats){
+        if(stats >= 400){
+            success = false
+        }
+    }
+  
+    let data = {
+        success,
+        message
+    }
+
+    if(results){
+        data.results = results
+    }
+    return res.status(stats).json(data)
+}
+
+module.exports = response
