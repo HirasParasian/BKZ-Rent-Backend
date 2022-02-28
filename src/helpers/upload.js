47c42fb46e920e271dc3e38fdbbda05a0db91196
HirasParasian <hirasparasian@gmail.com>
2022-02-28 12:27:53 +0700

    update : upload

--- src/helpers/upload.js
+++ src/helpers/upload.js
@@ -2,11 +2,11 @@ const multer = require("multer")
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads")
+    cb(null, "uploads/")
   },
   filename: function (req, file, cb) {
     const _file = file.originalname.split(".")
-    cb(null, "/" + _file[0] + "-" + Date.now() + "." + _file[1])
+    cb(null, "" + _file[0] + "-" + Date.now() + "." + _file[1])
   }
 })
 
