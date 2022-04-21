236d467135db3e36c6ac83bea33a4b0c371cd4f4
HirasParasian <hirasparasian@gmail.com>
2022-04-21 10:23:28 +0700

    feat : update

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,18 +1,29 @@
 const mysql = require("mysql")
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env
-var connection = mysql.createConnection(
-  {
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-  }
-)
+// var connection = mysql.createConnection(
+//   {
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     database: DB_NAME,
+//   }
+// )
 
-connection.connect(function (err) {
-  if (err)
-    throw err
-  console.log("Connected To Database!")
-})
+// connection.connect(function (err) {
+//   if (err)
+//     throw err
+//   console.log("Connected To Database!")
+// })
+
+const config = {
+	host : DB_HOST,
+	user : DB_USER,
+	password : DB_PASSWORD,
+	database : DB_NAME
+};
+
+const connection = mysql.createPool(config);
+
+console.log('DB Connected');
 
 module.exports = connection
